c4b0432e16bf950a0a98fe05607d9e1f5413a5be
Wells Wu <wells.wu4621@gmail.com>
2021-05-18 02:13:20 -0700

    finished question progression and submit process

--- assets/js/app.js
+++ assets/js/app.js
@@ -22,7 +22,7 @@ let questions = [
   },
   {
     question: ' What is the correct syntax for adding comments?',
-    choices: ['<!– I started this too late –>', '//Welp Lyfe', '# This was a large difficulty spike', '** Probably not getting much sleep tonight **', '/* Existential Crisis incoming */'],
+    choices: ['< !– I started this too late –>', '//Welp Lyfe', '# This was a large difficulty spike', '** Probably not getting much sleep tonight **', '/* Existential Crisis incoming */'],
     answer: '//Welp Lyfe',
   },
   {
@@ -32,37 +32,93 @@ let questions = [
   },
 ]
 // functions
+const endGame = () => {
+  score = time
+  document.getElementById('question').innerHTML = ''
+  document.getElementById('time').textContent = `Time: ${time}`
+  document.getElementById('score').textContent = `Score: ${score}`
+  document.getElementById('scoreForm').className = ''
+}
 const loadQuestion = () => {
   document.getElementById('question').innerHTML = ''
   let qElem = document.createElement('div')
   qElem.innerHTML = `
     <h3>Question ${current + 1}: ${questions[current].question}</h3>
     <ul class="collection">
-      <li class="collection-item waves-effect waves-green data-choice="${questions[current].choices[0]}">
+      <li class="collection-item waves-effect waves-green" data-choice="${questions[current].choices[0]}">
               A) ${questions[current].choices[0]}</li>
-      <li class="collection-item waves-effect waves-green data-choice="${questions[current].choices[1]}">
+      <li class="collection-item waves-effect waves-green" data-choice="${questions[current].choices[1]}">
               B) ${questions[current].choices[1]}</li>
-      <li class="collection-item waves-effect waves-green data-choice="${questions[current].choices[2]}">
+      <li class="collection-item waves-effect waves-green" data-choice="${questions[current].choices[2]}">
               C) ${questions[current].choices[2]}</li>
-      <li class="collection-item waves-effect waves-green data-choice="${questions[current].choices[3]}">
+      <li class="collection-item waves-effect waves-green" data-choice="${questions[current].choices[3]}">
               D) ${questions[current].choices[3]}</li>
-      <li class="collection-item waves-effect waves-green data-choice="${questions[current].choices[4]}">
+      <li class="collection-item waves-effect waves-green" data-choice="${questions[current].choices[4]}">
               E) ${questions[current].choices[4]}</li>
     </ul>
   `
   document.getElementById('question').append(qElem)
 }
 
+// Start Game Event
 document.getElementById('startBtn').addEventListener('click', () => {
   document.getElementById('startBtn').remove()
   timer = setInterval(() => {
     document.getElementById('time').textContent = `Time Left: ${time}`
-    time--
-    if (time < 0) {
+    if (time <= 0) {
       endGame()
       clearInterval(timer)
     }
+    time--
   }, 1000)
   loadQuestion()
 })
 
+document.addEventListener('click', event => {
+  if (event.target.classList.contains('collection-item')) {
+    if (event.target.dataset.choice !== questions[current].answer) {
+      time -= 15
+    }
+    console.log(event.target.dataset.choice)
+    current++
+    if (current >= questions.length) {
+      endGame()
+      clearInterval(timer)
+    }
+    else {
+      loadQuestion()
+    }
+  }
+})
+
+document.getElementById('submitButton').addEventListener('click', event => {
+  event.preventDefault()
+  let userInitials = document.getElementById('icon_prefix').value
+  let userScores = JSON.parse(localStorage.getItem('userScores')) || []
+  userScores.push({ userInitials, score })
+  localStorage.setItem('userScores', JSON.stringify(userScores))
+
+  userScores.sort((a, b) => b.score - a.score)
+
+  let tableHead = document.createElement('table')
+  tableHead.className = 'table highlight centered responsive-table'
+  tableHead.innerHTML = `
+        <thead>
+          <tr>
+              <th class="tableLabel">Initials</th>
+              <th class="tableLabel">Score</th>
+          </tr>
+        </thead>
+  `
+  let tableCont = document.createElement('tbody')
+  for (let i = 0; i < userScores.length; i++) {
+    tableCont.innerHTML += `
+      <tr>
+        <td class="tableValue">${userScores[i].userInitials}</td>
+        <td class="tableValue">${userScores[i].score}</td>
+      </tr>
+    `
+  }
+  tableHead.append(tableCont)
+  document.getElementById('question').append(tableHead)
+})
